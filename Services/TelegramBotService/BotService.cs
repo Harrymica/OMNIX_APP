c41fda583a59c3f4d67402fae7bc873abee31eb5
Harrymica <micachidubem007@gmail.com>
2024-12-05 08:33:29 +0100

    another

---
+++ Services/TelegramBotService/BotService.cs
@@ -0,0 +1,18 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.ReplyMarkups;
+
+namespace OMNIX_APP.Services.TelegramBotService
+{
+    public static class BotService
+    {
+       // private readonly TelegramBotClient _botClient;
+        
+        
+    }
+
+
+    
+}
