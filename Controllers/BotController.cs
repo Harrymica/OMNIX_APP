155bd623e7d243670c9fd1081dfb28f8e42e62e7
Harrymica <micachidubem007@gmail.com>
2024-12-14 10:40:07 +0100

    dfdhd

--- Controllers/BotController.cs
+++ Controllers/BotController.cs
@@ -46,7 +46,7 @@ namespace OMNIX_APP.Controllers
                 UpdateType.EditedMessage,
                     }
             };
-            await _bot.SetWebhook("https://omnix-app.onrender.com/bot", allowedUpdates: new UpdateType[]
+            await _bot.SetWebhook("https://omnix-app.onrender.com/api/bot", allowedUpdates: new UpdateType[]
                     {
                 UpdateType.Message,
                 UpdateType.EditedMessage,
